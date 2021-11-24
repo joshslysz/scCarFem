ffeb080cb4105a86e5af52c206ec0f74d330f724
joshslysz <80840036+joshslysz@users.noreply.github.com>
2021-11-24 14:11:45 -0400

    test

--- 4_All Lymphoid cells subset analyses.R
+++ 4_All Lymphoid cells subset analyses.R
@@ -76,14 +76,39 @@ alllymphoid.All.sig_markers <- alllymphoid.All.markers %>%
 
 write_csv(alllymphoid.All.sig_markers, file = "alllymphoid.All.sig_markers.csv")
 
-
 ################# T cells ######################################
 FeaturePlot(Alllymphoid.Carotid_Femoral_srt.int, features = "CD3D")
-VlnPlot(Alllymphoid.Carotid_Femoral_srt.int, split.by = "orig.ident", features = "CD3D")
+VlnPlot(Alllymphoid.Carotid_Femoral_srt.int, split.by = "orig.ident", features = "CD3D") # T cells and NKs
+FeaturePlot(Alllymphoid.Carotid_Femoral_srt.int, features = "CD2")
+VlnPlot(Alllymphoid.Carotid_Femoral_srt.int, split.by = "orig.ident", features = "CD2") #T cells and NKs
+FeaturePlot(Alllymphoid.Carotid_Femoral_srt.int, features = "CD69")
+VlnPlot(Alllymphoid.Carotid_Femoral_srt.int, split.by = "orig.ident", features = "CD69") #Exhausted T cells
+FeaturePlot(Alllymphoid.Carotid_Femoral_srt.int, features = "CD44")
+VlnPlot(Alllymphoid.Carotid_Femoral_srt.int, split.by = "orig.ident", features = "CD44") #Distinguishes memory and effector Ts from Naive
 
+################# Naive T cells ######################################
+FeaturePlot(Alllymphoid.Carotid_Femoral_srt.int, features = "CD28")
+VlnPlot(Alllymphoid.Carotid_Femoral_srt.int, split.by = "orig.ident", features = "CD28")
+FeaturePlot(Alllymphoid.Carotid_Femoral_srt.int, features = "CCR7")
+VlnPlot(Alllymphoid.Carotid_Femoral_srt.int, split.by = "orig.ident", features = "CCR7")
+
+
+###################################################################
 ################# CD8+ T cells ######################################
 FeaturePlot(Alllymphoid.Carotid_Femoral_srt.int, features = "CD8A")
 VlnPlot(Alllymphoid.Carotid_Femoral_srt.int, split.by = "orig.ident", features = "CD8A")
+FeaturePlot(Alllymphoid.Carotid_Femoral_srt.int, features = "CD8B")
+VlnPlot(Alllymphoid.Carotid_Femoral_srt.int, split.by = "orig.ident", features = "CD8B")
+FeaturePlot(Alllymphoid.Carotid_Femoral_srt.int, features = "GZMK")
+VlnPlot(Alllymphoid.Carotid_Femoral_srt.int, split.by = "orig.ident", features = "GZMK")
+FeaturePlot(Alllymphoid.Carotid_Femoral_srt.int, features = "GZMB")
+VlnPlot(Alllymphoid.Carotid_Femoral_srt.int, split.by = "orig.ident", features = "GZMB")
+FeaturePlot(Alllymphoid.Carotid_Femoral_srt.int, features = "IFNG")
+VlnPlot(Alllymphoid.Carotid_Femoral_srt.int, split.by = "orig.ident", features = "IFNG")
+FeaturePlot(Alllymphoid.Carotid_Femoral_srt.int, features = "TNF")
+VlnPlot(Alllymphoid.Carotid_Femoral_srt.int, split.by = "orig.ident", features = "TNF")
+
+
 
 #######################################################################
 ################# CD4+ T cells ######################################
@@ -91,14 +116,17 @@ FeaturePlot(Alllymphoid.Carotid_Femoral_srt.int, features = "CD4")
 VlnPlot(Alllymphoid.Carotid_Femoral_srt.int, split.by = "orig.ident", features = "CD4")
 FeaturePlot(Alllymphoid.Carotid_Femoral_srt.int, features = "CD28")
 VlnPlot(Alllymphoid.Carotid_Femoral_srt.int, split.by = "orig.ident", features = "CD28")
+FeaturePlot(Alllymphoid.Carotid_Femoral_srt.int, features = "CCR7")
+VlnPlot(Alllymphoid.Carotid_Femoral_srt.int, split.by = "orig.ident", features = "CCR7") # central memory T cells
 
 
-### CD4+ T reg CElls ### CLUSTER 10
+### CD4+ T reg CElls ### CLUSTER 9#### TEST TEST TEST
 FeaturePlot(Alllymphoid.Carotid_Femoral_srt.int, features = "IL2RA")
 VlnPlot(Alllymphoid.Carotid_Femoral_srt.int, split.by = "orig.ident", features = "IL2RA")
 FeaturePlot(Alllymphoid.Carotid_Femoral_srt.int, features = "Fox3P")
 VlnPlot(Alllymphoid.Carotid_Femoral_srt.int, split.by = "orig.ident", features = "FOX3P")
-
+FeaturePlot(Alllymphoid.Carotid_Femoral_srt.int, features = "Fox3P")
+VlnPlot(Alllymphoid.Carotid_Femoral_srt.int, split.by = "orig.ident", features = "FOX3P")
 ### CD4+ Th1 Cells ### DOES NOT SEEM TO PRESENT IN DATASET
 FeaturePlot(Alllymphoid.Carotid_Femoral_srt.int, features = "TBX21")
 VlnPlot(Alllymphoid.Carotid_Femoral_srt.int, split.by = "orig.ident", features = "TBX21")
@@ -141,6 +169,16 @@ VlnPlot(Alllymphoid.Carotid_Femoral_srt.int, split.by = "orig.ident", features =
 FeaturePlot(Alllymphoid.Carotid_Femoral_srt.int, features = "BCL6")
 VlnPlot(Alllymphoid.Carotid_Femoral_srt.int, split.by = "orig.ident", features = "BCL6")
 
+###Find Gamma sigma T cells  ### POssible cluster 14
+#cholesterol-related pathway genes: Cheng, Hsin-Yuan, et al. "Increased cholesterol content in gammadelta (γδ) T lymphocytes differentially regulates their activation." PloS one 8.5 (2013): e63746.
+FeaturePlot(Alllymphoid.Carotid_Femoral_srt.int, features = "ACAT1") #cholesterol esterification
+VlnPlot(Alllymphoid.Carotid_Femoral_srt.int, features = "ACAT1")
+FeaturePlot(Alllymphoid.Carotid_Femoral_srt.int, features = "ACAT2")#cholesterol esterification
+VlnPlot(Alllymphoid.Carotid_Femoral_srt.int, features = "ACAT2")
+FeaturePlot(Alllymphoid.Carotid_Femoral_srt.int, features = "TRERF1")#cholesterol utilization
+VlnPlot(Alllymphoid.Carotid_Femoral_srt.int, features = "TRERF1")
+FeaturePlot(Alllymphoid.Carotid_Femoral_srt.int, features = "IL6")
+VlnPlot(Alllymphoid.Carotid_Femoral_srt.int, features = "IL6")
 
 
 ###Find plasmacytoid DCs ### CLUSTER 10
@@ -157,7 +195,9 @@ Alllymphoid.Carotid_Femoral_srt.int <- CellCycleScoring(Alllymphoid.Carotid_Femo
                                                        g2m.features = cc.genes.updated.2019$g2m.genes)
 DimPlot(Alllymphoid.Carotid_Femoral_srt.int, group.by = "Phase", split.by = "orig.ident")
 
-###Natural Killer  Cells ###
+###Natural Killer  Cells ### CLUSTER 4 
+FeaturePlot(Alllymphoid.Carotid_Femoral_srt.int, features = "KLRC1")
+VlnPlot(Alllymphoid.Carotid_Femoral_srt.int, split.by = "orig.ident", features = "KLRC1")
 
 ### T reg CElls ### CLUSTER 10
 FeaturePlot(Alllymphoid.Carotid_Femoral_srt.int, features = "IL2RA")
@@ -166,6 +206,12 @@ FeaturePlot(Alllymphoid.Carotid_Femoral_srt.int, features = "Fox3P")
 VlnPlot(Alllymphoid.Carotid_Femoral_srt.int, split.by = "orig.ident", features = "FOX3P")
 
 
-
+####CLUSTER 12 ####
+FeaturePlot(Alllymphoid.Carotid_Femoral_srt.int, features = "CCR7")
+VlnPlot(Alllymphoid.Carotid_Femoral_srt.int, split.by = "orig.ident", features = "CCR7")
+FeaturePlot(Alllymphoid.Carotid_Femoral_srt.int, features = "CCL19")
+VlnPlot(Alllymphoid.Carotid_Femoral_srt.int, split.by = "orig.ident", features = "CCL19")
+FeaturePlot(Alllymphoid.Carotid_Femoral_srt.int, features = "CD83")
+VlnPlot(Alllymphoid.Carotid_Femoral_srt.int, split.by = "orig.ident", features = "CD83")
 
 
